0ef8ff8696decb421c4375d3c00760303c2c8665
James <106340884+ThePlumberJ@users.noreply.github.com>
2023-05-22 23:06:03 -0600

    Add files via upload

--- Proyecto/error.php
+++ Proyecto/error.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link rel="stylesheet" href="assents/css/estilo_principal.css">
+  <link rel="stylesheet" href="assents/css/error404.css">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
     crossorigin="anonymous"></script>
@@ -18,6 +18,47 @@
 </head>
 
 <body>
-    
+  <a  target="_blank">
+    <header class="top-header">
+  </header>
+  
+  <!--dust particel-->
+  <div>
+    <div class="starsec"></div>
+    <div class="starthird"></div>
+    <div class="starfourth"></div>
+    <div class="starfifth"></div>
+  </div>
+  <!--Dust particle end--->
+  
+  
+  <div class="lamp__wrap">
+    <div class="lamp">
+      <div class="cable"></div>
+      <div class="cover"></div>
+      <div class="in-cover">
+        <div class="bulb"></div>
+      </div>
+      <div class="light"></div>
+    </div>
+  </div>
+  <!-- END Lamp -->
+  <section class="error">
+    <!-- Content -->
+    <div class="error__content">
+      <div class="error__message message">
+        <h1 class="message__title">Page Not Found </h1>
+        <p class="message__text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
+      </div>
+      <div class="error__nav e-nav">
+        <a href="index.php" target="_blanck" class="e-nav__link"></a>
+      </div>
+    </div>
+    <!-- END Content -->
+  
+  </section>
+  
+    </a>
 </body>
-</html>
+
+<?php include "footer.php" ?>
